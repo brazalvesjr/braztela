31b24582519c9ef7faa94ec79b2dadeaeeff273e
Braz Junior <braztela@github.com>
2026-04-22 11:01:52 -0400

    fix v1.0.0: requires xbmc.python e input via xbmc.Keyboard (compat total)

--- plugin.video.braztela/default.py
+++ plugin.video.braztela/default.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 BRAZTELA - Addon Premium para Kodi
@@ -8,32 +7,47 @@ Versao 1.0.0
 import sys
 import os
 import json
-import xbmc
-import xbmcaddon
-import xbmcgui
-import xbmcplugin
-import xbmcvfs
-from urllib.parse import urlencode, parse_qsl
+
+try:
+    import xbmc
+    import xbmcaddon
+    import xbmcgui
+    import xbmcplugin
+    import xbmcvfs
+except ImportError:
+    sys.exit(0)
+
+try:
+    from urllib.parse import urlencode, parse_qsl
+except ImportError:
+    from urllib import urlencode
+    from urlparse import parse_qsl
 
 ADDON = xbmcaddon.Addon()
 ADDON_ID = ADDON.getAddonInfo('id')
 ADDON_NAME = "BRAZTELA"
 ADDON_VERSION = ADDON.getAddonInfo('version')
-ADDON_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo('path'))
-PROFILE_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
+
+try:
+    ADDON_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo('path'))
+    PROFILE_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
+except AttributeError:
+    ADDON_PATH = xbmc.translatePath(ADDON.getAddonInfo('path'))
+    PROFILE_PATH = xbmc.translatePath(ADDON.getAddonInfo('profile'))
+
 ICON = os.path.join(ADDON_PATH, 'icon.png')
 FANART = os.path.join(ADDON_PATH, 'fanart.jpg')
 
 if not os.path.exists(PROFILE_PATH):
     try:
         os.makedirs(PROFILE_PATH)
-    except:
+    except Exception:
         pass
 
 HANDLE = int(sys.argv[1]) if len(sys.argv) > 1 else -1
 BASE_URL = sys.argv[0] if len(sys.argv) > 0 else 'plugin://plugin.video.braztela/'
 
-# CLIENTES
+# CLIENTES EMBUTIDOS
 CLIENTES = {
     "TESTE": {"senha": "123456", "nome": "Cliente Teste", "ativo": True},
     "BRAZ": {"senha": "admin2026", "nome": "Braz Junior (Admin)", "ativo": True},
@@ -48,7 +62,7 @@ CLIENTES = {
     "CLIENTE010": {"senha": "braztv2026", "nome": "Cliente 010", "ativo": True},
 }
 
-# SERVIDORES
+# 20 SERVIDORES
 SERVIDORES = []
 for i in range(1, 21):
     SERVIDORES.append({
@@ -60,7 +74,10 @@ for i in range(1, 21):
 
 
 def log(msg):
-    xbmc.log("[BRAZTELA] " + str(msg), xbmc.LOGINFO)
+    try:
+        xbmc.log("[BRAZTELA] " + str(msg), xbmc.LOGINFO)
+    except Exception:
+        pass
 
 
 def build_url(action, **kwargs):
@@ -71,80 +88,110 @@ def build_url(action, **kwargs):
 
 def add_item(label, action, is_folder=True, plot="", **kwargs):
     li = xbmcgui.ListItem(label=label)
-    li.setArt({'icon': ICON, 'thumb': ICON, 'fanart': FANART})
+    try:
+        li.setArt({'icon': ICON, 'thumb': ICON, 'fanart': FANART})
+    except Exception:
+        pass
     if plot:
         try:
-            li.getVideoInfoTag().setPlot(plot)
-        except:
-            pass
+            info_tag = li.getVideoInfoTag()
+            info_tag.setPlot(plot)
+            info_tag.setTitle(label)
+        except Exception:
+            try:
+                li.setInfo('video', {'plot': plot, 'title': label})
+            except Exception:
+                pass
     url = build_url(action, **kwargs)
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=li, isFolder=is_folder)
 
 
-# AUTENTICACAO
+def get_input(heading, hidden=False):
+    """Usa xbmc.Keyboard - compativel com todas as versoes do Kodi."""
+    try:
+        kb = xbmc.Keyboard('', heading, hidden)
+        kb.doModal()
+        if not kb.isConfirmed():
+            return None
+        text = kb.getText()
+        if text is None:
+            return None
+        return text.strip()
+    except Exception as e:
+        log("Erro get_input: " + str(e))
+        return None
+
+
 def auth_file_path():
     return os.path.join(PROFILE_PATH, 'auth.json')
 
 
 def is_logged_in():
     f = auth_file_path()
-    if os.path.exists(f):
-        try:
-            with open(f, 'r') as fh:
-                data = json.load(fh)
-            cod = data.get('codigo', '')
-            if cod in CLIENTES and CLIENTES[cod].get('ativo', False):
-                return True
-        except:
-            pass
+    if not os.path.exists(f):
+        return False
+    try:
+        with open(f, 'r') as fh:
+            data = json.load(fh)
+        cod = data.get('codigo', '')
+        if cod in CLIENTES and CLIENTES[cod].get('ativo', False):
+            return True
+    except Exception:
+        pass
     return False
 
 
 def get_user_name():
     f = auth_file_path()
-    if os.path.exists(f):
-        try:
-            with open(f, 'r') as fh:
-                data = json.load(fh)
-            return data.get('nome', 'Usuario')
-        except:
-            pass
-    return 'Usuario'
+    if not os.path.exists(f):
+        return 'Usuario'
+    try:
+        with open(f, 'r') as fh:
+            data = json.load(fh)
+        return data.get('nome', 'Usuario')
+    except Exception:
+        return 'Usuario'
 
 
 def do_login():
     dialog = xbmcgui.Dialog()
-    dialog.ok(ADDON_NAME, "Bem-vindo ao BRAZTELA!\n\nInforme seu Codigo de Cliente e Senha de Acesso.\n(Fornecidos por Braz Junior)")
-    
-    codigo = dialog.input("Codigo do Cliente", type=xbmcgui.INPUT_ALPHANUMERIC)
-    if not codigo:
+    dialog.ok(ADDON_NAME,
+              "Bem-vindo ao BRAZTELA!\n" +
+              "Informe seu Codigo e Senha de Acesso.\n" +
+              "(Fornecidos por Braz Junior)")
+
+    codigo = get_input("Codigo do Cliente", hidden=False)
+    if codigo is None or codigo == "":
         return False
     codigo = codigo.upper().strip()
-    
-    senha = dialog.input("Senha de Acesso", type=xbmcgui.INPUT_ALPHANUMERIC, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-    if not senha:
+
+    senha = get_input("Senha de Acesso", hidden=True)
+    if senha is None or senha == "":
         return False
-    
+
     if codigo not in CLIENTES:
-        dialog.ok(ADDON_NAME, "Codigo invalido!")
+        dialog.ok(ADDON_NAME, "Codigo invalido!\nVerifique seu codigo de cliente.")
         return False
-    
+
     cliente = CLIENTES[codigo]
     if not cliente.get('ativo', False):
-        dialog.ok(ADDON_NAME, "Cliente bloqueado!\n\nContate Braz Junior.")
+        dialog.ok(ADDON_NAME, "Cliente bloqueado!\nContate Braz Junior.")
         return False
-    
+
     if cliente['senha'] != senha:
         dialog.ok(ADDON_NAME, "Senha incorreta!")
         return False
-    
+
     try:
         with open(auth_file_path(), 'w') as fh:
             json.dump({'codigo': codigo, 'nome': cliente['nome']}, fh)
-    except:
+    except Exception as e:
+        log("Erro ao salvar auth: " + str(e))
+
+    try:
+        dialog.notification(ADDON_NAME, "Bem-vindo, " + cliente['nome'] + "!", ICON, 3000)
+    except Exception:
         pass
-    
-    dialog.notification(ADDON_NAME, "Bem-vindo, " + cliente['nome'] + "!", ICON, 3000)
     return True
 
 
@@ -153,12 +200,14 @@ def do_logout():
     if os.path.exists(f):
         try:
             os.remove(f)
-        except:
+        except Exception:
             pass
-    xbmcgui.Dialog().notification(ADDON_NAME, "Sessao encerrada", ICON, 2000)
+    try:
+        xbmcgui.Dialog().notification(ADDON_NAME, "Sessao encerrada", ICON, 2000)
+    except Exception:
+        pass
 
 
-# CONTROLE PARENTAL
 def parental_file_path():
     return os.path.join(PROFILE_PATH, 'parental.json')
 
@@ -171,26 +220,26 @@ def show_parental_menu():
         try:
             with open(pf, 'r') as fh:
                 pin_atual = json.load(fh).get('pin')
-        except:
+        except Exception:
             pass
-    
+
     if pin_atual is None:
-        novo = dialog.input("Crie um PIN de 4 digitos", type=xbmcgui.INPUT_NUMERIC)
+        novo = get_input("Crie um PIN de 4 digitos", hidden=True)
         if novo and len(novo) >= 4:
             try:
                 with open(pf, 'w') as fh:
                     json.dump({'pin': novo}, fh)
                 dialog.ok(ADDON_NAME, "PIN parental criado!")
-            except:
+            except Exception:
                 dialog.ok(ADDON_NAME, "Erro ao salvar PIN")
         return
-    
+
     opcoes = ["Alterar PIN", "Desativar Controle Parental", "Cancelar"]
     escolha = dialog.select("Controle Parental", opcoes)
     if escolha == 0:
-        check = dialog.input("PIN atual", type=xbmcgui.INPUT_NUMERIC)
+        check = get_input("PIN atual", hidden=True)
         if check == pin_atual:
-            novo = dialog.input("Novo PIN (4 digitos)", type=xbmcgui.INPUT_NUMERIC)
+            novo = get_input("Novo PIN (4 digitos)", hidden=True)
             if novo and len(novo) >= 4:
                 with open(pf, 'w') as fh:
                     json.dump({'pin': novo}, fh)
@@ -198,15 +247,20 @@ def show_parental_menu():
         else:
             dialog.ok(ADDON_NAME, "PIN incorreto!")
     elif escolha == 1:
-        check = dialog.input("PIN para desativar", type=xbmcgui.INPUT_NUMERIC)
+        check = get_input("PIN para desativar", hidden=True)
         if check == pin_atual:
-            os.remove(pf)
+            try:
+                os.remove(pf)
+            except Exception:
+                pass
             dialog.ok(ADDON_NAME, "Controle parental desativado.")
 
 
-# SERVIDORES
 def get_current_server():
-    return ADDON.getSetting('servidor_atual') or '1'
+    try:
+        return ADDON.getSetting('servidor_atual') or '1'
+    except Exception:
+        return '1'
 
 
 def show_servers():
@@ -223,20 +277,25 @@ def show_servers():
     escolha = dialog.select("Selecione um Servidor", nomes)
     if escolha >= 0:
         s = ativos[escolha]
-        ADDON.setSetting('servidor_atual', str(s['id']))
-        dialog.notification(ADDON_NAME, "Servidor: " + s['nome'], ICON, 3000)
+        try:
+            ADDON.setSetting('servidor_atual', str(s['id']))
+        except Exception:
+            pass
+        try:
+            dialog.notification(ADDON_NAME, "Servidor: " + s['nome'], ICON, 3000)
+        except Exception:
+            pass
 
 
-# MENUS
 def show_main_menu():
     user = get_user_name()
     xbmcplugin.setPluginCategory(HANDLE, "BRAZTELA - " + user)
     xbmcplugin.setContent(HANDLE, 'videos')
-    
+
     add_item("[B][COLOR red]TV AO VIVO[/COLOR][/B]", "tv_live",
              plot="Acesse os canais de TV ao vivo do servidor atual.")
     add_item("[B][COLOR red]FILMES[/COLOR][/B]", "movies",
-             plot="Biblioteca completa de filmes com sinopse, capa e metadados.")
+             plot="Biblioteca completa de filmes com sinopse e metadados.")
     add_item("[B][COLOR red]SERIES[/COLOR][/B]", "series",
              plot="Series com episodios, sinopse e capa.")
     add_item("[B][COLOR yellow]TROCAR SERVIDOR[/COLOR][/B]", "servers",
@@ -247,7 +306,7 @@ def show_main_menu():
              plot="Configuracoes do addon.", is_folder=False)
     add_item("[COLOR gray]SAIR (Logout)[/COLOR]", "logout",
              plot="Encerrar sessao atual.", is_folder=False)
-    
+
     xbmcplugin.endOfDirectory(HANDLE)
 
 
@@ -255,27 +314,31 @@ def show_placeholder(title):
     xbmcplugin.setPluginCategory(HANDLE, title)
     xbmcplugin.setContent(HANDLE, 'videos')
     add_item("[I]Aguardando configuracao do servidor...[/I]", "main",
-             plot="O administrador esta configurando os servidores. Em breve voce tera acesso ao conteudo.",
+             plot="O administrador esta configurando os servidores.",
              is_folder=False)
     xbmcplugin.endOfDirectory(HANDLE)
 
 
 def router():
     if not is_logged_in():
-        if not do_login():
+        logged = do_login()
+        if not logged:
             xbmcplugin.endOfDirectory(HANDLE, succeeded=False)
             return
-    
+
     params = {}
     if len(sys.argv) > 2 and sys.argv[2]:
         try:
-            params = dict(parse_qsl(sys.argv[2][1:]))
-        except:
+            qs = sys.argv[2]
+            if qs.startswith('?'):
+                qs = qs[1:]
+            params = dict(parse_qsl(qs))
+        except Exception:
             params = {}
-    
+
     action = params.get('action', 'main')
     log("Acao: " + action)
-    
+
     if action == 'main':
         show_main_menu()
     elif action == 'tv_live':
@@ -286,10 +349,14 @@ def router():
         show_placeholder("SERIES")
     elif action == 'servers':
         show_servers()
+        xbmc.executebuiltin('Container.Refresh')
     elif action == 'parental':
         show_parental_menu()
     elif action == 'settings':
-        ADDON.openSettings()
+        try:
+            ADDON.openSettings()
+        except Exception:
+            pass
     elif action == 'logout':
         do_logout()
         xbmc.executebuiltin('Container.Refresh')
@@ -303,6 +370,6 @@ if __name__ == '__main__':
     except Exception as e:
         log("ERRO: " + str(e))
         try:
-            xbmcgui.Dialog().notification(ADDON_NAME, "Erro: " + str(e)[:50], xbmcgui.NOTIFICATION_ERROR, 5000)
-        except:
+            xbmcgui.Dialog().notification(ADDON_NAME, "Erro: " + str(e)[:60], xbmcgui.NOTIFICATION_ERROR, 5000)
+        except Exception:
             pass
